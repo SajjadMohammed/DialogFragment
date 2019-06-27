303b78863a8db26025860d4e976b5a0b6f76f4b5
SajjadMohammed <30750038+SajjadMohammed@users.noreply.github.com>
2019-06-27 23:25:06 +0300

    UpdateProject

--- app/src/main/java/com/sajjad/dialogfragment/DialogFragment.java
+++ app/src/main/java/com/sajjad/dialogfragment/DialogFragment.java
@@ -7,12 +7,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DialogFragment extends android.support.v4.app.DialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dialog_fragment,container,false);
+        View view  = inflater.inflate(R.layout.dialog_fragment,container,false);
+
+        // getting data from MainActivity
+        Bundle arguments=getArguments();
+        //
+        final EditText user_name=view.findViewById(R.id.user_name);
+        final EditText user_password=view.findViewById(R.id.user_password);
+        user_name.setText(arguments.getString("user_name"));
+
+        Button log_in=view.findViewById(R.id.log_in);
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // passing data to MainActivity
+                ((MainActivity)getActivity()).userName.setText(user_name.getText().toString());
+                ((MainActivity)getActivity()).password.setText(user_password.getText().toString());
+                dismiss();
+            }
+        });
+
+        return  view;
     }
 
     @Override
--- app/src/main/java/com/sajjad/dialogfragment/MainActivity.java
+++ app/src/main/java/com/sajjad/dialogfragment/MainActivity.java
@@ -1,13 +1,17 @@
 package com.sajjad.dialogfragment;
 
+import android.content.DialogInterface;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView userName,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,14 +19,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar supportToolbar=findViewById(R.id.supportv7_toolbar);
         setSupportActionBar(supportToolbar);
 
-
+        userName=findViewById(R.id.userName);
+         password=findViewById(R.id.password);
 
     }
 
     public void ShowDialogFragment(View view) {
         FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
 
-        DialogFragment dialogFragment=new DialogFragment();
+        final DialogFragment dialogFragment=new DialogFragment();
+        Bundle arguments=new Bundle();
+        arguments.putString("user_name","Sajjad");
+        dialogFragment.setArguments(arguments);
         dialogFragment.show(fragmentTransaction,null);
     }
 }
